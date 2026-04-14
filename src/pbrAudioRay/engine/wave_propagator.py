6ed30b6dc9d4667c5ff8bdaee51eb87ce4fb8780
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-15 01:21:36 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/wave_propagator.py
+++ src/pbrAudioRay/engine/wave_propagator.py
@@ -189,8 +189,9 @@ class WavePropagator:
         speed_of_sound = config.acoustic_domain.acoustic_shader.sound_speed
         # We'll sample IR at sample rate
         # find length of IR
-        max_samples = int(max(rays, key=lambda x: x.length) * self.sample_rate / speed_of_sound)
-        ir_amp = np.zeros(max(samples))
+        max_length = max(rays, key=lambda x: x.length)
+        samples = int(max_length * self.sample_rate / speed_of_sound)
+        ir_amp = np.zeros(samples)
         
         for ray in rays:
             if ray is None:
