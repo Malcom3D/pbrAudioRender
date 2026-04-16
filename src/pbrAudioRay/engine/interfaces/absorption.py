33139d7a822366eee02d327d66c2af732f9944dc
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-16 14:51:39 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/interfaces/absorption.py
+++ src/pbrAudioRay/engine/interfaces/absorption.py
@@ -80,7 +80,7 @@ class AbsorptionInterface:
         """
     
         # Generate logarithmically spaced frequencies points for average
-        print(low_freq, high_freq, num=100, endpoint=True)
+        print(low_freq, high_freq)
         frequency = np.geomspace(low_freq, high_freq, num=100, endpoint=True)
 
         # Get shader data
