81c9865efdaaa0631067f8c832e127137c9c98e7
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-16 12:34:27 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/interfaces/absorption.py
+++ src/pbrAudioRay/engine/interfaces/absorption.py
@@ -51,7 +51,7 @@ class AbsorptionInterface:
             if not phase == None:
                 ray.phase = np.angle(np.exp(1j * (ray.phase + phase))
 
-        elif shader.acoustic_properties and shader.acoustic_properties.absorption:
+        elif hasattr(shader, 'acoustic_properties') and hasattr(shader.acoustic_properties, 'absorption'):
             coeff, phase = shader.acoustic_properties.absorption.get_avg_coeffs(low_freq, high_freq)
             ray.energy *= (1 - coeff)
             if not phase == None:
