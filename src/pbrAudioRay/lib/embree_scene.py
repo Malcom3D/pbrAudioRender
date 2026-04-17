ed2eb18bd090f575065a877ccd5544cc1445e7fd
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-18 00:04:47 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/lib/embree_scene.py
+++ src/pbrAudioRay/lib/embree_scene.py
@@ -100,7 +100,7 @@ class EmbreeScene:
             for key in objects.keys():
                 if objects[key].obj_idx == obj_config.idx:
                     task_mesh += [self._get_obj_mesh(objects[key], obj_config, src_pos, out_pos)]
-                    meshes_results = compute(*task_mesh)
+        meshes_results = compute(*task_mesh)
 
         # Add all acoustic objects with their actual obj_ids
         for obj_mesh, obj_idx, name in meshes_results:
@@ -201,9 +201,9 @@ class EmbreeScene:
          if source_config.type == 'SPHERE':
              src_radius = source_config.size
              if src_radius > 0:
-                 print('_get_source_mesh: ok' , source_config.name, 'combo: ', self.combo)
+                 print('_get_source_mesh: ok' , source_config.name, 'combo: ', self.combo, src_radius)
                  return source_pos, trimesh.creation.icosphere(subdivisions=2, radius=src_radius, transform=[[1, 0, 0, source_pos[0]],[0, 1, 0, source_pos[1]],[0, 0, 1, source_pos[2]],[0, 0, 0, 1]])
-         print('_get_source_mesh: no' , source_config.name, 'combo: ', self.combo)
+         print('_get_source_mesh: no' , source_config.name, 'combo: ', self.combo, src_radius)
          return source_pos, None
 
     def _get_output_mesh(self, output_idx: int): 
