9fb96f4ddc9bf5fdaaba557d60c76d2b361cac91
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-18 12:16:55 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/wave_propagator.py
+++ src/pbrAudioRay/engine/wave_propagator.py
@@ -67,6 +67,7 @@ class WavePropagator:
 
 #        directions = self._generate_initial_directions(n_rays, source_pos, output_pos)
         directions = self._generate_isotropic_directions(source_pos, output_pos, n_rays)
+        print('directions: ', directions)
         intercect = scene.run(source_pos, directions)
 
         print(intersect)
