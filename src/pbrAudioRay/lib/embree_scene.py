76aab850557e335e4691bd483acc91be43d7db75
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-19 20:32:43 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/lib/embree_scene.py
+++ src/pbrAudioRay/lib/embree_scene.py
@@ -162,22 +162,17 @@ class EmbreeScene:
         self.density = np.append(self.density, np.full((faces.shape[0],), density, dtype=np.float32))
         if obj_idx >= 0:
             # Get Object AcousticShader
-            coeffs, phases = config.acoustic_domain.acoustic_shader.absorption.get_bands_avg(self.freq_bands)
+            coeffs, phases = obj_config.acoustic_shader.absorption.get_bands_avg(self.freq_bands)
             self.absorption = np.append(self.absorption, np.full((faces.shape[0],2,n_bands), [coeffs.tolist(), phases.tolist()], dtype=np.float32))
 
-            coeffs, phases = config.acoustic_domain.acoustic_shader.refraction.get_bands_avg(self.freq_bands)
+            coeffs, phases = obj_config.acoustic_shader.refraction.get_bands_avg(self.freq_bands)
             self.refraction = np.append(self.refraction, np.full((faces.shape[0],2,n_bands), [coeffs.tolist(), phases.tolist()], dtype=np.float32))
 
-            coeffs, phases = config.acoustic_domain.acoustic_shader.reflection.get_bands_avg(self.freq_bands)
+            coeffs, phases = obj_config.acoustic_shader.reflection.get_bands_avg(self.freq_bands)
             self.reflection = np.append(self.reflection, np.full((faces.shape[0],2,n_bands), [coeffs.tolist(), phases.tolist()], dtype=np.float32))
 
-            coeffs, phases = config.acoustic_domain.acoustic_shader.scattering.get_bands_avg(self.freq_bands)
+            coeffs, phases = obj_config.acoustic_shader.scattering.get_bands_avg(self.freq_bands)
             self.scattering = np.append(self.scattering, np.full((faces.shape[0],2,n_bands), [coeffs.tolist(), phases.tolist()], dtype=np.float32))
-
-#            self.absorption = np.append(self.absorption, np.full((faces.shape[0],2,n_bands), [config.acoustic_domain.acoustic_shader.absorption.get_bands_avg(self.freq_bands)], dtype=np.float32)
-#            self.refraction = np.append(self.refraction, np.full((faces.shape[0],2,n_bands), [config.acoustic_domain.acoustic_shader.refraction.get_bands_avg(self.freq_bands)], dtype=np.float32)
-#            self.reflection = np.append(self.reflection, np.full((faces.shape[0],2,n_bands), [config.acoustic_domain.acoustic_shader.reflection.get_bands_avg(self.freq_bands)], dtype=np.float32)
-#            self.scattering = np.append(self.scattering, np.full((faces.shape[0],2,n_bands), [config.acoustic_domain.acoustic_shader.scattering.get_bands_avg(self.freq_bands)], dtype=np.float32)
 
     @delayed
     def _get_obj_mesh(self, object: Any, obj_config: Any, src_pos: np.ndarray, out_pos: np.ndarray):
