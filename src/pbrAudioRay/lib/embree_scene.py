f5f24d326f2196218b814b5142c758129874b66b
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-19 15:01:51 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/lib/embree_scene.py
+++ src/pbrAudioRay/lib/embree_scene.py
@@ -154,8 +154,10 @@ class EmbreeScene:
         n_bands = len(self.freq_bands)
 
         # Get Material Info
-        self.sound_speed = np.append(self.sound_speed, np.full((faces.shape[0],), obj_config.acoustic_shader.sound_speed, dtype=np.float32)
-        self.density = np.append(self.density, np.full((faces.shape[0],), obj_config.acoustic_shader.density, dtype=np.float32)
+        sound_speed = obj_config.acoustic_shader.sound_speed
+        self.sound_speed = np.append(self.sound_speed, np.full((faces.shape[0],), sound_speed, dtype=np.float32)
+        density = obj_config.acoustic_shader.density
+        self.density = np.append(self.density, np.full((faces.shape[0],), density, dtype=np.float32)
         if obj_idx >= 0:
             # Get Object AcousticShader
             coeffs, phases = config.acoustic_domain.acoustic_shader.absorption.get_bands_avg(self.freq_bands)
