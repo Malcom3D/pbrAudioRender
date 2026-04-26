9e3c4c6f3b800340225226fee64394db79e61069
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-26 23:13:56 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/diff_path_tracer.py
+++ src/pbrAudioRay/engine/diff_path_tracer.py
@@ -189,8 +189,8 @@ class DiffPathTracer:
         # Filter direction, hit_points and phases on energy termination
         termination_energy = 1e-6
         termination_mask = appended_energies > termination_energy
-        new_energies = appended_energies[termination_mask]
-        new_phases = appended_phases[termination_mask]
+        new_energies = appended_energies[termination_mask].reshape(-1,1)
+        new_phases = appended_phases[termination_mask].reshape(-1,1)
         new_directions = appended_directions[termination_mask.reshape(-1,)]
         new_origins = appended_origins[termination_mask.reshape(-1,)]
 
