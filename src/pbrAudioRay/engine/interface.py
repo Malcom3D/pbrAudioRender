980fa01d809a72a91833eaaa860e925fc4f252f2
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-29 16:33:26 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/engine/interface.py
+++ src/pbrAudioRay/engine/interface.py
@@ -110,7 +110,7 @@ class InterfaceManager:
 
         rays_energies = absorbed_energy = reflected_energy = scattered_energy = ray_data.energies
         rays_phases = absorbed_phases = reflected_phases = scattered_phases = ray_data.phases
-        reflected_directions = scattered_directions = np.zeros((origins.shape[0],3), dtype=np.float32)
+        reflected_directions = scattered_directions = None
         incident_angles = 1
 
         if enable_absorption:
@@ -132,23 +132,24 @@ class InterfaceManager:
             roughness_factor = self.acoustic_scene.get_roughness(mask=prim_ids)
             scattered_energy, scattered_phases, scattered_directions = self.scattering_interface.compute(rays_energies, rays_phases, normals, roughness_factor, scat_coeffs, scat_phases)
 
-        # Energy conservation check
-        total_out = reflected_energy + scattered_energy + absorbed_energy
-        delta_energies = abs(total_out - rays_energies)
-        delta_mask = delta_energies  > 1e-10
-        if not np.all(delta_mask):
-            total_out[~delta_mask] = rays_energies[~delta_mask] + 1e-10
-        # Normalize to ensure energy conservation
-        scale = rays_energies / total_out
-        reflected_energy *= scale
-        scattered_energy *= scale
-        absorbed_energy *= scale
+        absorbed_energy, scattered_energy, reflected_energy = self._check_energy_conservation(rays_energies, absorbed_energy, reflected_energy, scattered_energy)
 
         # Append hit_points for origins, directions, energies and phases
-        appended_origins = np.append(hit_points, hit_points, axis=0).astype(np.float32)
-        appended_directions = np.append(reflected_directions, scattered_directions, axis=0).astype(np.float32)
-        appended_energies = np.append(reflected_energy, scattered_energy, axis=0).astype(np.float32)
-        appended_phases = np.append(reflected_phases, scattered_phases, axis=0).astype(np.float32)
+        if isinstance(reflected_directions, np.ndarray) and isinstance(scattered_directions, np.ndarray):
+            appended_origins = np.append(hit_points, hit_points, axis=0).astype(np.float32)
+            appended_directions = np.append(reflected_directions, scattered_directions, axis=0).astype(np.float32)
+            appended_energies = np.append(reflected_energy, scattered_energy, axis=0).astype(np.float32)
+            appended_phases = np.append(reflected_phases, scattered_phases, axis=0).astype(np.float32)
+        elif isinstance(reflected_directions, np.ndarray) and not isinstance(scattered_directions, np.ndarray):
+            appended_origins = hit_points
+            appended_directions = reflected_directions
+            appended_energies = reflected_energy
+            appended_phases = reflected_phases
+        elif not isinstance(reflected_directions, np.ndarray) and isinstance(scattered_directions, np.ndarray):
+            appended_origins = hit_points
+            appended_directions = scattered_directions
+            appended_energies = scattered_energy
+            appended_phases = scattered_phases
         
         # Filter direction, hit_points and phases on energy termination
         termination_energy = 1e-6 # config.termination.energy_threshold
@@ -164,3 +165,28 @@ class InterfaceManager:
 
         self.current_interactions += 1
         return new_ray_data
+
+    def _check_energy_conservation(self, rays_energies: np.ndarray, absorbed_energy: np.ndarray = None, reflected_energy: np.ndarray = None, scattered_energy: np.ndarray = None):
+        # Energy conservation check
+        total_out += rays_energies
+        if isinstance(absorbed_energy, np.ndarray):
+            total_out += absorbed_energy
+        if isinstance(reflected_energy, np.ndarray):
+            total_out += reflected_energy
+        if isinstance(scattered_energy, np.ndarray):
+            total_out += scattered_energy
+
+        delta_energies = abs(total_out - rays_energies)
+        delta_mask = delta_energies > 1e-10
+        if not np.all(delta_mask):
+            total_out[~delta_mask] = rays_energies[~delta_mask] + 1e-10
+        # Normalize to ensure energy conservation
+        scale = rays_energies / total_out
+        if isinstance(absorbed_energy, np.ndarray):
+            absorbed_energy *= scale
+        if isinstance(reflected_energy, np.ndarray):
+            reflected_energy *= scale
+        if isinstance(scattered_energy, np.ndarray): 
+            scattered_energy *= scale
+
+        return absorbed_energy, reflected_energy, scattered_energy
