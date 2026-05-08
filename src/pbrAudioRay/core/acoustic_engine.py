63270f96e36fa0b0018218bdc80dace31ed11f51
Malcom3D <Malcom3D@users.noreply.github.com>
2026-05-08 22:51:30 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- src/pbrAudioRay/core/acoustic_engine.py
+++ src/pbrAudioRay/core/acoustic_engine.py
@@ -139,7 +139,7 @@ class AcousticEngine:
             self._compute_frame(frame_idx)
 
         # interpolate x bands_idx IRs for wave_propagators[index].combo
-        ir_task = [self._interpolate_ir([wave_propagators[index].combo) for index in wave_propagators.keys()]
+        ir_task = [self._interpolate_ir(wave_propagators[index].combo) for index in wave_propagators.keys()]
         ir_results = compute(*ir_tasks)
 
     @delayed
